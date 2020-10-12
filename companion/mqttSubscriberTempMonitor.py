13740eb4ecc76044949b0b5556ca9a9eba456ecb
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2020-10-12 15:59:38 +0200

    working subscriber

--- companion/mqttSubscriberTempMonitor.py
+++ companion/mqttSubscriberTempMonitor.py
@@ -1,16 +1,21 @@
 import logging
 
+import serial
 import paho.mqtt.client as mqtt
 
 class MQTTClient(mqtt.Client):
-    topic = "5h/+/temp"
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, topic, serial_port, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.serial = serial_port
+        self.topic = f"5h/{topic}/temp"
 
     def on_connect(self, mqttc, obj, flags, rc):
         logging.info("Connected to server with return code {}".format(rc))
-        mqttc.subscribe(MQTTClient.topic)
+        mqttc.subscribe(self.topic)
+
+        self.max_t = 28.0
+        self.min_t = 27.0
 
     def on_disconnect(self, userdata, rc):
         if rc != 0:
@@ -19,18 +24,35 @@ class MQTTClient(mqtt.Client):
 
     def on_message(self, mqttc, userdata, msg):
         logging.info(f"msg: {msg.payload.decode()} from topic: {msg.topic}")
+        try:
+            temp = float(msg.payload.decode())
+            if temp > self.max_t:
+                action = 'c'
+            elif temp < self.min_t:
+                action = 'h'
+            else:
+                action = 's'
+
+            response = ''
+            while response != action:
+                self.serial.write(action.encode())
+                response = self.serial.read(1).decode()
+
+        except ValueError:
+            logging.warning("Invalid temperature")
 
 
 def main():
-    client = MQTTClient(protocol=mqtt.MQTTv311)
+    with serial.Serial("COM6") as s:
+        client = MQTTClient(protocol=mqtt.MQTTv311, topic="fano", serial_port=s)
 
-    client.connect("mqtt.ssh.edu.it")
-    logging.info("Connected to MQTT server")
+        client.connect("mqtt.ssh.edu.it")
+        logging.info("Connected to MQTT server")
 
-    try:
-        client.loop_forever()
-    except KeyboardInterrupt:
-        client.disconnect()
+        try:
+            client.loop_forever()
+        except KeyboardInterrupt:
+            client.disconnect()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
